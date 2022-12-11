8e01b5b021f936986c3999f0cd10929db344e3af
adithya-yadav <badriadithya7396@gmail.com>
2022-12-11 10:57:32 +0530

    setbutton

--- src/App.js
+++ src/App.js
@@ -4,21 +4,38 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [load,setLoad] = useState()
+  const [load,setLoad] = useState(true)
+  const [error,setError] = useState(null)
+  const [btn,setBtn] = useState(false)
   async function fetchMoviesHandler() {
-    setLoad(false)
-    const res = await fetch("https://swapi.dev/api/films/");
-    const data = await res.json();
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releseData: movieData.relese_date,
-      };
-    });
+    try{
+      setLoad(false)
+      const res = await fetch("https://swapi.dev/api/film/");
+      if(!res.ok){
+        setTimeout(()=>{
+          setBtn(true)
+        },5000)
+        throw new Error("SomeThing went wrong... Retrying")
+      }
+      const data = await res.json();
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releseData: movieData.relese_date,
+        };
+      });
+      setLoad(true)
+      setMovies(transformedMovies);
+    }catch(err){
+      setError(err.message)
+    }
+    
+  }
+  function setErrorHandler(){
+    setError(null)
     setLoad(true)
-    setMovies(transformedMovies);
   }
   return (
     <React.Fragment>
@@ -26,7 +43,8 @@ function App() {
           <button>Fetch Movies</button>
         </section>
         <section>
-          {load===false && <h2>Loading...please wait..</h2>}
+          {error && <> <h2>{error}</h2> {btn && <button onClick={setErrorHandler}>Cancel</button>}</>}
+          {!load && !error&& <h2>Loading...please wait..</h2>}
           {load && <MovieList movies={movies} />}
         </section>
 
